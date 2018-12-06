384fd3eb53f927313287ef3d49fd54e8727a18aa
aleleonel <aleleonel@gmail.com>
2018-12-06 16:30:57 -0200

    criação de novas views About - Contact e suas respectivas estruturas html, (inseridas no urls.py path)tbm uma pequena alteração no arquivo .gitignore

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -3,7 +3,23 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    
-    return render(request , "home_page.html", {})
+    context = {
+        "title": "Pagina Principal",
+        "content": "Esta é a pagina principal"
+    }
+    return render(request , "home_page.html", context)
 
+def about_page(request):
+    context = {
+        "title": "Pagina Sobre",
+        "content": "Esta é a página Sobre"
+    }
+    return render(request, "about_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title": "Página de Contato",
+        "content": "Esta é a página de contato"
+    }
+    return render(request, "contact_page.html", context)
     
