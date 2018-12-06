d304ef23da3925aded21791972e008ee76630a6e
aleleonel <aleleonel@gmail.com>
2018-12-06 16:56:08 -0200

    criação de subpasta dentro da pasta template django_ecommerce/template/<subpasta>/<arquivo>.html

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -7,19 +7,19 @@ def home_page(request):
         "title": "Pagina Principal",
         "content": "Esta é a pagina principal"
     }
-    return render(request , "home_page.html", context)
+    return render(request , "home/home_page.html", context)
 
 def about_page(request):
     context = {
         "title": "Pagina Sobre",
         "content": "Esta é a página Sobre"
     }
-    return render(request, "about_page.html", context)
+    return render(request, "about/about_page.html", context)
 
 def contact_page(request):
     context = {
         "title": "Página de Contato",
         "content": "Esta é a página de contato"
     }
-    return render(request, "contact_page.html", context)
+    return render(request, "contact/contact_page.html", context)
     
