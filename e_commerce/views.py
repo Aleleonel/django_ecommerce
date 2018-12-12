b56829249e817fa8eea7b3630aec624405f76f69
aleleonel <aleleonel@gmail.com>
2018-12-12 13:53:34 -0200

    Aletrações e merge no master

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,9 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-<<<<<<< HEAD
-=======
 from .forms import ContactForm
->>>>>>> create_form
 
 
 def home_page(request):
@@ -21,12 +18,10 @@ def about_page(request):
     return render(request, "about/about_page.html", context)
 
 def contact_page(request):
-<<<<<<< HEAD
     context = {
         "title": "Página de Contato",
         "content": "Esta é a página de contato"
     }
-=======
     contact_form = ContactForm(request.POST or None)
     context = {
         "title": "Contato",
@@ -39,6 +34,5 @@ def contact_page(request):
     # Apenas uma verificação no terminal
     ###if request.method == 'POST':
         #print(request.POST)
->>>>>>> create_form
     return render(request, "contact/contact_page.html", context)
     
