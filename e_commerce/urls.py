6653c4a7fb9cd65829f8ff7be833346e1a8b26bc
aleleonel <aleleonel@gmail.com>
2018-12-26 00:22:28 -0200

    static cdn configuirado

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,5 +1,4 @@
 """e_commerce URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
@@ -13,8 +12,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
+
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -25,3 +29,7 @@ urlpatterns = [
     path('register', register_page),
     path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    
